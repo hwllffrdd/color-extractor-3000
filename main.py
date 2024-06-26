63148135f655c5d472f564b21aa4919753b51fdb
Hwllffrdd <weight@atlas.cz>
2024-06-26 12:30:34 +0200

    Website with the function created

--- main.py
+++ main.py
@@ -1,36 +1,61 @@
+from flask import Flask, request, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap5
 import numpy as np
 from PIL import Image
 from collections import Counter
 
-image = Image.open('image.jpg')
-img_array = np.array(image)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yoursecretkey'
+Bootstrap5(app)
 
-dtype = [('R', np.uint8), ('G', np.uint8), ('B', np.uint8)]
 
-structured_array = np.zeros((img_array.shape[0], img_array.shape[1]), dtype=dtype)
+def extract_common_colors(image_path, num_colors=3):
+    """
+    Extracts the most common colors from an image.
 
-structured_array['R'] = img_array[:,:,0]
-structured_array['G'] = img_array[:,:,1]
-structured_array['B'] = img_array[:,:,2]
+    Parameters:
+    image_path (str): The path to the image file.
+    num_colors (int): The number of most common colors to return.
 
-flattened_tuples = [tuple(row) for row in structured_array.reshape(-1)]
+    Returns:
+    list of str: A list containing the hexadecimal color codes of the most common colors.
+    """
+    image = Image.open('image.jpg')
+    img_array = np.array(image)
 
-counter = Counter(flattened_tuples)
+    dtype = [('R', np.uint8), ('G', np.uint8), ('B', np.uint8)]
 
-first_value = counter.most_common(3)[0][0]
-second_value = counter.most_common(3)[1][0]
-third_value = counter.most_common(3)[2][0]
+    structured_array = np.zeros((img_array.shape[0], img_array.shape[1]), dtype=dtype)
 
-r1, g1, b1 = first_value
-r2, g2, b2 = second_value
-r3, g3, b3 = third_value
+    structured_array['R'] = img_array[:,:,0]
+    structured_array['G'] = img_array[:,:,1]
+    structured_array['B'] = img_array[:,:,2]
 
-hex_color_first = f"#{r1:02x}{g1:02x}{b1:02x}"
-hex_color_second = f"#{r2:02x}{g2:02x}{b2:02x}"
-hex_color_third = f"#{r3:02x}{g3:02x}{b3:02x}"
+    flattened_tuples = [tuple(row) for row in structured_array.reshape(-1)]
 
-print(hex_color_first)
-print(hex_color_second)
-print(hex_color_third)
+    counter = Counter(flattened_tuples)
+    most_common_colors = counter.most_common(num_colors)
+
+    hex_colors = [f"#{r:02x}{g:02x}{b:02x}" for r, g, b in (color[0] for color in most_common_colors)]
+
+    return hex_colors
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        num_colors = int(request.form.get('num_colors', 3))
+        if file and file.filename != '':
+            image = Image.open(file.stream)
+            colors = extract_common_colors(image, num_colors)
+            return render_template('results.html', colors=colors)
+        else:
+            return redirect(request.url)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
